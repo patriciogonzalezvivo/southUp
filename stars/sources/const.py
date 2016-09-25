c6d3fd8fa64f617a08d614596f0bf8b14e82f972
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-09-24 21:26:07 -0400

    constelations parsed and fixed

--- stars/sources/const.py
+++ stars/sources/const.py
@@ -3,19 +3,32 @@
 import geojson
 import os
 
-CONST_INFO = "./constellations.json"
+CONST_LABELS = "./constellations.json"
 CONST_LINES = "./constellations.lines.json" 
+CONST_OUT = "./const.json"
 
-const_info = geojson.load(open(CONST_INFO))
-const_lines = geojson.load(open(CONST_LINES))
+const = {}
+const['lines'] = geojson.load(open(CONST_LINES))
+const['labels'] = geojson.load(open(CONST_LABELS))
+
+# Fix and take geometries
+for feature in const['lines'].features:
+    for line in feature.geometry.coordinates:
+        lng_prev = 0
+        for coord in line:
+            lng_diff = coord[0] - lng_prev
+            if abs(lng_diff) > 180:
+                lng_offset = 0
+                if lng_diff > 0:
+                    lng_offset = -360
+                else:
+                    lng_offset = 360
+                coord[0] += lng_offset
+            lng_prev = coord[0]
 
 
-geometries = {}
-for feature in const_lines.features:
-    geometries[feature.id] = feature.geometry
 
-for feature in const_info.features:
-    feature.geometry = geometries[feature.id] 
 
-for feature in const_info.features:
-    print feature.geometry.coordinates
+file = open(CONST_OUT, 'w')
+file.write(geojson.dumps(const, sort_keys=True))
+file.close()
